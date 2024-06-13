242d179982e62bc1d54f61ae7e2d8059832dbeb5
Zaeem <zaeemmasoud@gmail.com>
2024-06-13 12:23:58 +0300

    changes in button css

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,37 @@ function App() {
     <div className="h-screen w-full" style={{ backgroundColor: color }}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 bg-slate-200 mx-auto w-fit p-3 rounded-2xl">
         <button
-          className="p-3 mx-3 w-20 rounded-xl shadow-xl text-white bg-red-600"
+          className="p-3 m-3 w-20 rounded-xl shadow-xl text-white bg-red-600"
           onClick={() => setColor("red")}
         >
           Red
         </button>
         <button
-          className="p-3 mx-3 w-20 rounded-xl shadow-xl text-white bg-blue-600"
+          className="p-3 m-3 w-20 rounded-xl shadow-xl text-white bg-blue-600"
           onClick={() => setColor("blue")}
         >
           Blue
         </button>
         <button
-          className="p-3 mx-3 w-20 rounded-xl shadow-xl text-white bg-green-600"
+          className="p-3 m-3 w-20 rounded-xl shadow-xl text-white bg-green-600"
           onClick={() => setColor("green")}
         >
           Green
         </button>
         <button
-          className="p-3 mx-3 w-20 rounded-xl shadow-xl text-white bg-black"
+          className="p-3 m-3 w-20 rounded-xl shadow-xl text-white bg-black"
           onClick={() => setColor("black")}
         >
           Black
         </button>
         <button
-          className="p-3 mx-3 w-20 rounded-xl shadow-xl text-white bg-pink-500"
+          className="p-3 m-3 w-20 rounded-xl shadow-xl text-white bg-pink-500"
           onClick={() => setColor("pink")}
         >
           Peach
         </button>
         <button
-          className="p-3 mx-3 w-20 rounded-xl shadow-xl text-white bg-slate-500"
+          className="p-3 m-3 w-20 rounded-xl shadow-xl text-white bg-slate-500"
           onClick={() => setColor("gray")}
         >
           Grey
